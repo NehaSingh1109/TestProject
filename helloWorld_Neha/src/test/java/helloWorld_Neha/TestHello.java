2653bfcb619df6b1878a99b556e060296141a7b8
NehaSingh1109 <nsnehasingh670@gmail.com>
2021-07-17 23:32:35 +0530

    First Commit

---
+++ helloWorld_Neha/src/test/java/helloWorld_Neha/TestHello.java
@@ -0,0 +1,15 @@
+package helloWorld_Neha;
+
+import org.testng.annotations.Test;
+
+public class TestHello {
+	
+	
+	@Test
+	public void testHelloWorld() {
+		System.out.println("Hello World");
+	
+
+	}
+
+}
